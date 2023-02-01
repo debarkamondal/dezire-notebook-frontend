0b94ec6f47c67589771648d5b6a6b2794280dd69
debarkamondal <debarkamondal@gmail>
2023-02-01 23:46:53 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
@@ -7,14 +7,21 @@ import Navbar from "./components/Navbar";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
     children: [
+      {
+        index: true,
+        element: (
+          <>
+            <Home />
+          </>
+        ),
+      },
       {
         path: "/about",
         element: (
           <>
-            <About />
             <Navbar />
+            <About />
           </>
         ),
       },
@@ -26,6 +33,7 @@ function App() {
   return (
     <>
       {/* <Navbar /> */}
+      <Outlet />
       <RouterProvider router={router} />
     </>
   );
