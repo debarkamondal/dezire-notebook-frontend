ce15471006065b56f50dd7de6aea2c5c9411a5c5
debarkamondal <debarkamondal@gmail>
2023-02-05 02:39:28 +0530

    working on fetching all notes

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,13 +1,43 @@
 // import React from "react";
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const state = {
-    name: "Hola",
-    hobby: "Guitar",
-  };
+  const notesInit = [
+    {
+      _id: "63debfe7dc3a4d55bb4f0491",
+      user: "63d5f000ddf52da7b65b19c8",
+      title: "Dummy title",
+      description: "dummy description",
+      tag: "general",
+      date: "2023-02-04T20:28:23.872Z",
+      __v: 0,
+    },
+    {
+      _id: "63debfeddc3a4d55bb4f0493",
+      user: "63d5f000ddf52da7b65b19c8",
+      title: "Dummy title2",
+      description: "dummy description2",
+      tag: "general",
+      date: "2023-02-04T20:28:29.696Z",
+      __v: 0,
+    },
+    {
+      _id: "63debff1dc3a4d55bb4f0495",
+      user: "63d5f000ddf52da7b65b19c8",
+      title: "Dummy title23",
+      description: "dummy description23",
+      tag: "general",
+      date: "2023-02-04T20:28:33.722Z",
+      __v: 0,
+    },
+  ];
+
+  const [notes, setNotes] = useState(notesInit);
   return (
-    <NoteContext.Provider value={state}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
+    </NoteContext.Provider>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,56 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
 const Home = () => {
+  const context = useContext(noteContext);
+  const { notes, setNotes } = context;
   return (
-    <>
-      <h1>This is Home</h1>
-      {/* <Outlet /> */}
-    </>
+    <div className="container">
+      <h1 className="text-center my-4">Add a note</h1>
+      <form classsame=" my-2">
+        <div className="mb-3">
+          <label htmlFor="noteTitle" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="noteTitle"
+            aria-describedby="noteTitle"
+            placeholder="Enter you title here"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="noteTag" className="form-label">
+            Tag
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="noteTag"
+            aria-describedby="noteTag"
+            placeholder="Enter you tag here"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Description
+          </label>
+          <textarea
+            type="text"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check"></div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <h2 className="text-center">Your Notes</h2>
+      {notes.map((note) => {
+        return note.title;
+      })}
+    </div>
   );
 };
 
